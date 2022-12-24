e62812e56c4c2db328c76d1832034ab59993cff7
amirb <amirbourvine@campus.technion.ac.il>
2022-12-24 14:27:10 +0200

    hash with chain written but not working so i neglect it

--- hash_test.cpp
+++ hash_test.cpp
@@ -2,7 +2,7 @@
 // Created by amirb on 17/12/2022.
 //
 
-#include "Hash_Table.h"
+#include "Hash_Table_Chain.h"
 #include <iostream>
 
 
@@ -11,15 +11,17 @@ bool isEqual(const int& t1, const int& t2){
 }
 
 int main(){
-    Hash_Table<int> hash(isEqual);
-    for(int i = 0; i<37; i++){
-        hash.insert(i*i+5, i+1);
-        hash.printHash();
-    }
-    if(hash.get(31,905).status()==StatusType::SUCCESS){
-        cout<<"SUCCESS: " << hash.get(31,905).ans()<<endl;
-    }
-    else{
-        cout<<"FAILURE"<<endl;
-    }
+    Hash_Table_Chain<int> hash(isEqual);
+
+
+//    for(int i = 0; i<37; i++){
+//        hash.insert(i*i+5, i+1);
+//        hash.printHash();
+//    }
+//    if(hash.get(31,905).status()==StatusType::SUCCESS){
+//        cout<<"SUCCESS: " << hash.get(31,905).ans()<<endl;
+//    }
+//    else{
+//        cout<<"FAILURE"<<endl;
+//    }
 }
